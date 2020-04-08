7046c0983cd4a404ba7b65c08853d117ff6e8c4a
Severin <s.ryberg@fz-juelich.de>
2020-04-08 16:38:24 +0200

    Updated version number in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='glaes',
-    version='1.1.3',
+    version='1.1.4',
     author='Severin Ryberg',
     url='https://github.com/FZJ-IEK3-VSA/glaes',
     packages=find_packages(),
