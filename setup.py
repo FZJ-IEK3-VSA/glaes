b03375e282afe6f62b6cdd7fac7bffadbbf10b27
Severin <s.ryberg@fz-juelich.de>
2020-05-19 13:56:00 +0200

    upgraded to v1.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='glaes',
-    version='1.1.7',
+    version='1.1.8',
     author='Severin Ryberg',
     url='https://github.com/FZJ-IEK3-VSA/glaes',
     packages=find_packages(),
