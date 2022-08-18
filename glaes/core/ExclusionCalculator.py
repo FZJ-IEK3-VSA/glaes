2052e679159e3c4afabef8860571d30eae942e4c
David Franzmann <d.franzmann@fz-juelich.de>
2022-08-18 09:27:08 +0200

    indicateFeaturesMultiprocess (sync with GEOKIT!)

--- glaes/core/ExclusionCalculator.py
+++ glaes/core/ExclusionCalculator.py
@@ -840,7 +840,7 @@ class ExclusionCalculator(object):
     # General excluding functions
     def excludeRasterType(s, source, value=None, buffer=None, resolutionDiv=1,
                           intermediate=None, prewarp=False, invert=False, mode="exclude",
-                          minSize=None, threshold=50, default=False, **kwargs):
+                          minSize=None, threshold=50, default=False, _spawnNewProcess=False, **kwargs):
         """Exclude areas based off the values in a raster datasource
 
         Parameters:
@@ -944,6 +944,9 @@ class ExclusionCalculator(object):
             If a string is passed as source, it will be written into the 
             sourcePath as well as the _exclusionStr instead of the actual 
             source. Defaults to False.
+        
+        _spawnNewProcess: boolean, option.
+            If true, the core calulation is spaned inside a new process to free RAM from heavy osgeo objects.
 
         kwargs
             * All other keyword arguments are passed on to a call to
@@ -1007,18 +1010,52 @@ class ExclusionCalculator(object):
 
                 source = s.region.warp(
                     source, returnMatrix=False, **prewarpArgs)
-            # Indicate on the source
-            indications = (
-                s.region.indicateValues(
-                    source,
-                    value,
-                    buffer=buffer,
-                    resolutionDiv=resolutionDiv,
-                    forceMaskShape=True,
-                    applyMask=False,
-                    **kwargs) * 100
-            ).astype(np.uint8)
+            
+            # calculate the actual exclusions
+                
+            #try in new process:
+            try:
+                if _spawnNewProcess:
+                    manager = multiprocessing.Manager()
+                    sharedDict = manager.dict()
 
+                    p = multiprocessing.Process(
+                        target=s.region.indicateValuesMultiprocess,
+                        args=(
+                            source,
+                        ),
+                        kwargs={**{
+                        'value': value,
+                        'buffer': buffer,
+                        'resolutionDiv': resolutionDiv,
+                        'forceMaskShape': True,
+                        'applyMask': False,
+                        'sharedDict': sharedDict,
+                        }, **kwargs}
+                    )
+                    p.start()
+                    p.join()
+
+                    indications_raw = sharedDict['indications']
+                    indications = (indications_raw * 100).astype(np.uint8)
+                    manager.shutdown()
+                else:
+                    raise ValueError('Nothing to worry, just jump to exception')
+            
+            except:
+                warn('Memory efficient way failed, returning to save method')
+                    # Indicate on the source
+                indications = (
+                    s.region.indicateValues(
+                        source,
+                        value,
+                        buffer=buffer,
+                        resolutionDiv=resolutionDiv,
+                        forceMaskShape=True,
+                        applyMask=False,
+                        **kwargs) * 100
+                ).astype(np.uint8)
+            
             # drop all isolated areas below minSize if given
             if not minSize == None:
 
