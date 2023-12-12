f904ab4de5c85248f6e363fdcde12acb82222e31
t.pelser <t.pelser@fz-juelich.de>
2023-12-12 09:48:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='glaes',
-    version='1.2.1',
+    version='1.2.2',
     author='GLAES Developer Team',
     url='https://github.com/FZJ-IEK3-VSA/glaes',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "geokit>=1.2.8",
-        "gdal>2.0.0,<3.0.0",
+        "geokit>=1.4.0",
+        "gdal==3.4.*",
         "numpy",
         "descartes",
         "pandas",
